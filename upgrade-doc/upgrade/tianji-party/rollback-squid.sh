6e53a727b39c1fe0873b377f2d6f422c232f76dd
Your Name <you@example.com>
2026-04-09 15:39:00 +0800

    aa

--- upgrade-doc/upgrade/tianji-party/rollback-squid.sh
+++ upgrade-doc/upgrade/tianji-party/rollback-squid.sh
@@ -4,15 +4,10 @@
 
 export BACKUP_DIR=/root/backup/20260413/tianji-party
 
-### 1. transproxy php upgrade
 
-cp /opt/nginx/html/transproxy_admin/app/Console/Commands/RemoveDup.php $BACKUP_DIR/
-cp /opt/nginx/html/transproxy_admin/app/Http/Controllers/Api/ThreatController.php $BACKUP_DIR/
+### 1. squid.conf rollback
 
-\cp -f $UPGRADE_DIR/transproxy/RemoveDup.php /opt/nginx/html/transproxy_admin/app/Console/Commands/RemoveDup.php
-\cp -f $UPGRADE_DIR/transproxy/ThreatController.php /opt/nginx/html/transproxy_admin/app/Http/Controllers/Api/ThreatController.php
 
-chmod 664 /opt/nginx/html/transproxy_admin/app/Console/Commands/RemoveDup.php
-chown nginx.www-group /opt/nginx/html/transproxy_admin/app/Console/Commands/RemoveDup.php
-chmod 664 /opt/nginx/html/transproxy_admin/app/Http/Controllers/Api/ThreatController.php
-chown nginx.www-group /opt/nginx/html/transproxy_admin/app/Http/Controllers/Api/ThreatController.php
+\cp -f $BACKUP_DIR/squid.conf /opt/squid/etc/squid.conf
+chown squid.squid /opt/squid/etc/squid.conf
+
--- upgrade-doc/upgrade/tianji-party/rollback.sh
+++ upgrade-doc/upgrade/tianji-party/rollback.sh
@@ -4,15 +4,14 @@
 
 export BACKUP_DIR=/root/backup/20260413/tianji-party
 
-### 1. transproxy php upgrade
+### 1. transproxy php rollback
 
-cp /opt/nginx/html/transproxy_admin/app/Console/Commands/RemoveDup.php $BACKUP_DIR/
-cp /opt/nginx/html/transproxy_admin/app/Http/Controllers/Api/ThreatController.php $BACKUP_DIR/
-
-\cp -f $UPGRADE_DIR/transproxy/RemoveDup.php /opt/nginx/html/transproxy_admin/app/Console/Commands/RemoveDup.php
-\cp -f $UPGRADE_DIR/transproxy/ThreatController.php /opt/nginx/html/transproxy_admin/app/Http/Controllers/Api/ThreatController.php
+\cp -f $BACKUP_DIR/RemoveDup.php /opt/nginx/html/transproxy_admin/app/Console/Commands/RemoveDup.php
+\cp -f $BACKUP_DIR/ThreatController.php /opt/nginx/html/transproxy_admin/app/Http/Controllers/Api/ThreatController.php
 
 chmod 664 /opt/nginx/html/transproxy_admin/app/Console/Commands/RemoveDup.php
 chown nginx.www-group /opt/nginx/html/transproxy_admin/app/Console/Commands/RemoveDup.php
 chmod 664 /opt/nginx/html/transproxy_admin/app/Http/Controllers/Api/ThreatController.php
 chown nginx.www-group /opt/nginx/html/transproxy_admin/app/Http/Controllers/Api/ThreatController.php
+
+
