71ca786c5865bf335d2e5b8d63fe8628024047e4
Your Name <you@example.com>
2026-04-09 18:26:33 +0800

    aa

--- stat.sh
+++ stat.sh
@@ -9,25 +9,23 @@ awk -v start_ts="$START_TS" '
 BEGIN {
     print "["
     first = 1
-    # 月份映射
     split("Jan Feb Mar Apr May Jun Jul Aug Sep Oct Nov Dec", m, " ")
     for (i=1; i<=12; i++) months[m[i]] = sprintf("%02d", i)
 }
 {
-    # 1. 处理时间戳 (取第4列，例如 [09/Apr/2026:17:48:49)
+    # 修正点：将 [\/:] 改为 [/:]，消除警告
     t_str = substr($4, 2)
-    split(t_str, t_parts, "[\/:]")
-    # 转换为 Unix 秒数
+    split(t_str, t_parts, "[/:]")
+    
+    # 确保时间戳转换正常
     cur_ts = mktime(t_parts[3] " " months[t_parts[2]] " " t_parts[1] " " t_parts[4] " " t_parts[5] " " t_parts[6])
 
-    # 2. 检查是否在 5 分钟范围内
     if (cur_ts >= start_ts) {
         ip = $1
         user = $3
-        # 根据你的 logformat，流量字段 %<st 在第 10 列
+        # 对应你的 HAsquidlog 格式中第 10 列的 %<st
         bytes = $10 
         
-        # 3. 累加数据
         key = ip SUBSEP user
         count[key]++
         sum_bytes[key] += bytes
@@ -36,17 +34,13 @@ BEGIN {
 END {
     for (key in count) {
         split(key, parts, SUBSEP)
-        u_ip = parts[1]
-        u_name = (parts[2] == "-" ? "anonymous" : parts[2])
-        
         if (!first) print ","
         
         s_bytes = sum_bytes[key]
-        s_kb = s_bytes / 1024
-        s_mb = s_kb / 1024
+        s_mb = s_bytes / 1024 / 1024
 
         printf "  {\n    \"ip\": \"%s\",\n    \"user\": \"%s\",\n    \"request_count\": %d,\n    \"total_bytes\": %d,\n    \"size_mb\": %.4f\n  }", 
-                u_ip, u_name, count[key], s_bytes, s_mb
+                parts[1], (parts[2] == "-" ? "anonymous" : parts[2]), count[key], s_bytes, s_mb
         first = 0
     }
     print "\n]"
