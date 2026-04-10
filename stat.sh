4256bca0a2778e7fc30be56b2dcac4a8f843651b
Your Name <you@example.com>
2026-04-10 10:45:36 +0800

    aa

--- stat.sh
+++ stat.sh
@@ -1,10 +1,27 @@
 #!/bin/bash
 
+# 1. 获取传入的参数，默认为 5
+# 使用 printf "%d" 强制将输入转换为整数（如果是空或非数字会变为 0）
+raw_input=${1:-5}
+if [[ "$raw_input" =~ ^[0-9]+$ ]]; then
+    MINUTES=$((raw_input))
+else
+    MINUTES=0
+fi
+
+# 2. 限制最大值为 60
+if [ "$MINUTES" -gt 60 ]; then
+    MINUTES=60
+fi
+
+echo $MINUTES
+exit
+
 # 日志路径
 LOG_FILE="/opt/squid/var/log/access.log"
 
-# 获取 5 分钟前的 Unix 时间戳
-START_TS=$(date -d "5 minutes ago" +%s)
+# 获取 $MINUTES 分钟前的 Unix 时间戳
+START_TS=$(date -d "$MINUTES minutes ago" +%s)
 
 awk -v start_ts="$START_TS" '
 BEGIN {
